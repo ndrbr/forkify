2201ecf40d0b0496568d4231e4520a81e5b32ea4
ndrbr <andrey.burdak@gmail.com>
2023-04-01 22:33:04 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4c93e3c0-88d3-4667-841c-7915ef401df0';
+export const MODAL_CLOSE_SEC = 2.5;
+// export const FAKE_ID = '5ed6604591c99cdc054bcc13';
